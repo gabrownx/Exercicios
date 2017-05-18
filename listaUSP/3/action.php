964c188395f38e0ef964fd577922ab2a2ffe909d
Gabriel <gabrielima.si@gmail.com>
2017-05-18 15:37:50 -0300

    Comentários do código

--- listaUSP/3/action.php
+++ listaUSP/3/action.php
@@ -1,13 +1,11 @@
 <?php
-echo 'Os ' .$_POST['num']. ' primeiros ímpares são: ';
+echo 'Os ' .$_POST['num']. ' primeiros ímpares são: '; // Imprime a mensagem inicial
 
-while ($i != $_POST['num']){
-    $x++;
-if (($x % 2) != 0){
-    // é impar
-    $i++;
-    echo  $x. ' '; 
+while ($i != $_POST['num']){ // Enquanto a quantidade de $i for diferente do número recebido
+    $x++; //Incrementa o número $x
+if (($x % 2) != 0){ // Testa se o número $x é impar
+    $i++; // Incrementa a contagem de números ímpares
+    echo  $x. ' ';  // Imprime o número ímpar atual
     }   
-
 }
 ?>
