7fdc57791b852ad80488063e40b768cc82ff76fa
Gabriel <gabrielima.si@gmail.com>
2017-05-19 13:24:39 -0300

    Adicionado dump e impressão de array

--- listaUSP/5/action.php
+++ listaUSP/5/action.php
@@ -1,7 +1,9 @@
 <?php
 
+var_dump($_POST['dia']);
 //OPCIONAL - Quantos produtos foram vendidos
-for ($i = 0; $i <= 30; $i++){
+/*for ($i = 0; $i <= 30; $i++){
 echo $_POST['dia$i'];
-}
+}*/
+print_r(array_values ($dia));
 ?>
