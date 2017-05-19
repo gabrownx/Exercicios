c7cb45d9d995ce62d0edb1edd9406973f9be706e
Gabriel <gabrielima.si@gmail.com>
2017-05-19 16:34:44 -0300

    Mudanças nos arrays

--- listaUSP/5/action.php
+++ listaUSP/5/action.php
@@ -2,8 +2,10 @@
 
 var_dump($_POST['dia']);
 //OPCIONAL - Quantos produtos foram vendidos
-/*for ($i = 0; $i <= 30; $i++){
-echo $_POST['dia$i'];
-}*/
+
+for ($i = 0; $i <= 30; $i++){
+echo $dia[$i];
+}
 print_r(array_values ($dia));
+echo array_sum($dia);
 ?>
